a03bd5bdcd3c24f1cd60555f2afba8028326fa3a
psychevisions <xammie.seun@gmail.com>
2019-02-19 10:28:51 +0100

    First wave

---
+++ script.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Sample</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+<body>
+    <div>
+        Font Samples:
+        <p>
+            click to delete <button class="btn btn-ig btn-success"> Text<i class="fa fa-plus" style="color: red"></i></button> <br>
+            click to add <i class="fa fa-check"></i> <br>
+            click to logout <i class="fa fa-lock"></i> 
+        </p>
+
+    </div>
+    
+</body>
+</html>
